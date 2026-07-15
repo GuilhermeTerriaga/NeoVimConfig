d2e6d1ff779a04e74358e733074c3249369160ad
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2026-07-15 13:30:05 -0300

    add jest to test suite

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -4,15 +4,16 @@ return {
 		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
 		"nvim-neotest/neotest-python",
 		"jfpedroza/neotest-elixir",
+		"marilari88/neotest-jest",
 	},
 	config = function()
 		require("neotest").setup({
 			adapters = {
 				require("neotest-python"),
 				require("neotest-elixir"),
+				require("neotest-jest"),
 			},
 		})
 		local wk = require("which-key")
